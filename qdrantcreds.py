65615f6ed5fa03bd54c448640108b7fe0c1168de
kavita.jain <kavita.jain@digital.datamatics.com>
2025-07-10 18:59:14 +0530

    qdrant file updated

--- qdrantcreds.py
+++ qdrantcreds.py
@@ -1,8 +1,8 @@
 from qdrant_client import QdrantClient
 
 qdrant_client = QdrantClient(
-    url="https://fe3fb69d-7252-4712-bea2-8f330aa3c42d.eu-west-2-0.aws.cloud.qdrant.io:6333", 
-    api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.nDfLwT6DVgBPg93vQcpoLM4IkYbZAY-Sx6spflHFS9A",
+    url="your-qdrant-url", 
+    api_key="your-qdrant-api-key",
 )
 
 print(qdrant_client.get_collections())
